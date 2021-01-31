063aab60aba57e9ee3b5b9414eb876f1671a451b
Daynan Crull <daynan@gmail.com>
2021-01-30 23:35:21 -0500

    init push

---
+++ sensors/temp.py
@@ -0,0 +1,56 @@
+# This program stores data collected by a BME280 temp,humidity,pressure sensor controlled
+# by a Raspberry Pi 3 via an I2C protocol. The data is written to a .csv file stored in an SD card.
+
+from pathlib import Path
+import RPi.GPIO as GPIO
+import csv, time, sys
+# from os import path
+# import Adafruit_BME280 as BME280
+from sensors.ada_fruit import BME280
+
+OPATH = Path('home','pi','data','temp_humidty_baro.csv')
+
+def write_csv(data,opath=OPATH):
+    try:
+        with open(opath, 'a') as f:
+            w = csv.writer(f)
+            w.writerow(data)
+        f.close()
+    except IOError:
+        time.sleep(5)
+        write_csv(data, opath)
+    return
+
+
+def main(sensor):
+    while True:
+        temp_list = []
+        pres_list = []
+        humid_list = []
+
+        t_end = time.time() + 30
+        while time.time() < t_end:
+            temp_list.append(sensor.read_temperature())
+            pres_list.append(sensor.read_pressure() / 100)
+            humid_list.append(sensor.read_humidity())
+
+        data = (time.time(),
+                sum(temp_list) * 1.0 / len(temp_list),
+                sum(pres_list) * 1.0 / len(pres_list),
+                sum(humid_list) * 1.0 / len(humid_list))
+
+        write_csv(data)
+    return
+
+
+if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
+    print('reading sensors...')
+    try:
+        sensor = BME280()
+
+    except Exception as e:
+        print(e)
+        exit()
+
+    main(sensor)
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='Temp-sensors',
+      version='1.0',
+      author='Daynan Crull',
+      packages=find_packages())
